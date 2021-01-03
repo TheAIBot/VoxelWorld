9aee0757c5a031269e391e0ddfb60e84a8fca49e
Cake <andreasuranus@hotmail.com>
2021-01-03 17:24:31 +0100

    also record assembly generated

--- VoxelBench/Program.cs
+++ VoxelBench/Program.cs
@@ -6,6 +6,7 @@ using BenchmarkDotNet.Running;
 
 namespace VoxelBench
 {
+    [DisassemblyDiagnoser(printSource: true, maxDepth: 5)]
     public class VoxelBencher
     {
         private readonly SeedsInfo seeds;
@@ -15,7 +16,7 @@ namespace VoxelBench
         public VoxelBencher()
         {
             this.seeds = new SeedsInfo(32, 32);
-            this.systemData = new VoxelSystemData(100, 0.3f, new PlanetGen(3, 1.0f, 3.0f, 1.0f));
+            this.systemData = new VoxelSystemData(100, 0.03f, new PlanetGen(3, 1.0f, 3.0f, 1.0f));
 
             this.grid = new VoxelGrid(new Vector3(0, 0, 0), systemData);
         }
