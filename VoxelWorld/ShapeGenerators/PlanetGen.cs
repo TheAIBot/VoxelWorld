d762c1c05366d730e65044a2109f042ffa734520
Cake <andreasuranus@hotmail.com>
2021-01-04 00:30:20 +0100

    turbulence only have to make dot product first. Can multiply afterwards to acheieve same result

--- VoxelWorld/ShapeGenerators/PlanetGen.cs
+++ VoxelWorld/ShapeGenerators/PlanetGen.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace VoxelWorld
 {
@@ -27,22 +29,64 @@ namespace VoxelWorld
             float sphere = SphereGen.GetValue(pos, PlanetRadius);
             float noise = Turbulence(pos * NoiseFrequency, seedsPtr, sphere);
 
-            return noise * NoiseWeight + sphere;
+            return noise + sphere;
         }
 
         private unsafe float Turbulence(Vector4 pos, float* seedsPtr, float sphereValue)
         {
-            sphereValue = sphereValue / NoiseWeight;
+            float* prods = stackalloc float[SEED_COUNT];
+
+            bool firstNoiseGen = true;
             float noiseSum = 0.0f;
-            float scale = 2.0f;
-            for (int i = 0; i < TURBULENCE_COUNT; i++)
+            float scale = 2.0f * NoiseWeight;
+            for (int q = 0; q < TURBULENCE_COUNT; q++)
             {
                 if (MathF.Abs(sphereValue + noiseSum) > scale)
                 {
                     break;
                 }
+
+                if (firstNoiseGen)
+                {
+                    firstNoiseGen = false;
+
+                    Vector128<float> pos128 = pos.AsVector128();
+                    Vector256<float> pos256 = Vector256.Create(pos128, pos128);
+
+                    int index = 0;
+                    for (int i = 0; i < Seeds.Seeds.Length; i += Vector256<float>.Count * 4)
+                    {
+                        //Load 8 seed vectors
+                        Vector256<float> s12 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 0);
+                        Vector256<float> s34 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 1);
+                        Vector256<float> s56 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 2);
+                        Vector256<float> s78 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 3);
+                        
+                        
+                        Vector256<float> ps12 = Avx.DotProduct(pos256, s12, 0b0111_1000);//[2,_,_,_,1,_,_,_]
+                        Vector256<float> ps34 = Avx.DotProduct(pos256, s34, 0b0111_0100);//[_,4,_,_,_,3,_,_]
+                        Vector256<float> ps56 = Avx.DotProduct(pos256, s56, 0b0111_0010);//[_,_,6,_,_,_,5,_]
+                        Vector256<float> ps78 = Avx.DotProduct(pos256, s78, 0b0111_0001);//[_,_,_,8,_,_,_,7]
+
+                        Vector256<float> ps1234 = Avx.Add(ps12, ps34);//[2,4,_,_,1,3,_,_]
+                        Vector256<float> ps5678 = Avx.Add(ps56, ps78);//[_,_,6,8,_,_,5,7]
+                        Vector256<float> ps = Avx.Add(ps1234, ps5678);//[2,4,6,8,1,3,5,7]
+
+                        Avx.Store(prods + index, ps);
+                        index += Vector256<float>.Count;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < Seeds.Seeds.Length / 4; i += Vector256<float>.Count)
+                    {
+                        var fgre = Avx.LoadVector256(prods + i);
+                        Avx.Store(prods + i, Avx.Add(fgre, fgre));
+                    }
+                }
+
                 scale *= 0.5f;
-                noiseSum += scale * XYZRandomGen.GetNoise(Seeds, seedsPtr, pos);
+                noiseSum += scale * XYZRandomGen.GetNoise(Seeds, prods);
                 pos *= 2.0f;
             }
 
--- VoxelWorld/ShapeGenerators/XYZRandomGen.cs
+++ VoxelWorld/ShapeGenerators/XYZRandomGen.cs
@@ -47,7 +47,7 @@ namespace VoxelWorld
     internal static class XYZRandomGen
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal unsafe static float GetNoise(SeedsInfo seeds, float* seedsPtr, Vector4 pos)
+        internal unsafe static float GetNoise(SeedsInfo seeds, float* seedsPtr)
         {
             //Cos approximation constants
             Vector256<float> const0_25 = Vector256.Create(0.25f);
@@ -56,26 +56,10 @@ namespace VoxelWorld
             Vector256<float> const0_5 = Vector256.Create(0.5f);
             Vector256<float> const_noSign = Vector256.Create(0x7fffffff).AsSingle();
 
-            Vector128<float> pos128 = pos.AsVector128();
-            Vector256<float> pos256 = Vector256.Create(pos128, pos128);
             Vector256<float> noise = Vector256<float>.Zero;
-            for (int i = 0; i < seeds.Seeds.Length; i += Vector256<float>.Count * 4)
+            for (int i = 0; i < seeds.Seeds.Length / 4; i += Vector256<float>.Count)
             {
-                //Load 8 seed vectors
-                Vector256<float> s12 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 0);
-                Vector256<float> s34 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 1);
-                Vector256<float> s56 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 2);
-                Vector256<float> s78 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 3);
-                
-                
-                Vector256<float> ps12 = Avx.DotProduct(pos256, s12, 0b0111_1000);//[2,_,_,_,1,_,_,_]
-                Vector256<float> ps34 = Avx.DotProduct(pos256, s34, 0b0111_0100);//[_,4,_,_,_,3,_,_]
-                Vector256<float> ps56 = Avx.DotProduct(pos256, s56, 0b0111_0010);//[_,_,6,_,_,_,5,_]
-                Vector256<float> ps78 = Avx.DotProduct(pos256, s78, 0b0111_0001);//[_,_,_,8,_,_,_,7]
-
-                Vector256<float> ps1234 = Avx.Add(ps12, ps34);//[2,4,_,_,1,3,_,_]
-                Vector256<float> ps5678 = Avx.Add(ps56, ps78);//[_,_,6,8,_,_,5,7]
-                Vector256<float> ps = Avx.Add(ps1234, ps5678);//[2,4,6,8,1,3,5,7]
+                Vector256<float> ps = Avx.LoadVector256(seedsPtr + i);
 
                 //Cos approximation
                 Vector256<float> cosApprox = Avx.Multiply(ps, consttp);
