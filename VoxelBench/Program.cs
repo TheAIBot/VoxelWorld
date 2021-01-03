9142e24f46b2be36e1d4cda5171be15499a69c4e
Cake <andreasuranus@hotmail.com>
2021-01-04 00:28:36 +0100

    added more different voxel grid scenarios

--- VoxelBench/Program.cs
+++ VoxelBench/Program.cs
@@ -10,19 +10,26 @@ namespace VoxelBench
     public class VoxelBencher
     {
         private readonly SeedsInfo seeds;
-        private readonly VoxelSystemData systemData;
-        private readonly VoxelGrid grid;
+        private readonly VoxelSystemData systemDataPlanet;
+        private readonly VoxelSystemData systemDataRandom;
+        private readonly VoxelGrid gridPlanet;
+        private readonly VoxelGrid gridRandom;
 
         public VoxelBencher()
         {
             this.seeds = new SeedsInfo(32, 32);
-            this.systemData = new VoxelSystemData(100, 0.03f, new PlanetGen(3, 1.0f, 3.0f, 1.0f));
+            this.systemDataPlanet = new VoxelSystemData(100, 0.03f, new PlanetGen(3, 1.0f, 3.0f, 1.0f));
+            this.systemDataRandom = new VoxelSystemData(100, 0.03f, new PlanetGen(3, 1.0f, 30.0f, 1.0f));
 
-            this.grid = new VoxelGrid(new Vector3(0, 0, 0), systemData);
+            this.gridPlanet = new VoxelGrid(new Vector3(0, 0, 0), systemDataPlanet);
+            this.gridRandom = new VoxelGrid(new Vector3(0, 0, 0), systemDataRandom);
         }
 
         [Benchmark]
-        public void MakeGrid() => grid.Randomize();
+        public void MakeGridPlanet() => gridPlanet.Randomize();
+
+        [Benchmark]
+        public void MakeGridRandom() => gridRandom.Randomize();
     }
 
     public class Program
