5a712567cedc5703eedba55cdcfa84131f541854
Cake <andreasuranus@hotmail.com>
2021-01-04 02:41:43 +0100

    first noise is always needed so can move dot products out of loop

--- VoxelWorld/ShapeGenerators/PlanetGen.cs
+++ VoxelWorld/ShapeGenerators/PlanetGen.cs
@@ -33,9 +33,41 @@ namespace VoxelWorld
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private unsafe float Turbulence(Vector4 pos, float* seedsPtr, float* prods, float sphereValue)
+        private unsafe float Turbulence(Vector4 pos, float* seedsPtr, float* noiseValues, float sphereValue)
         {
-            bool firstNoiseGen = true;
+            Vector128<float> pos128 = pos.AsVector128();
+            Vector256<float> pos256 = Vector256.Create(pos128, pos128);
+
+            for (int i = 0; i < Seeds.GetSeedsCount(); i += Vector256<float>.Count)
+            {
+                //Load 8 seed vectors
+                Vector256<float> s12 = Avx.LoadVector256(seedsPtr + i * 4 + Vector256<float>.Count * 0);
+                Vector256<float> s34 = Avx.LoadVector256(seedsPtr + i * 4 + Vector256<float>.Count * 1);
+                Vector256<float> s56 = Avx.LoadVector256(seedsPtr + i * 4 + Vector256<float>.Count * 2);
+                Vector256<float> s78 = Avx.LoadVector256(seedsPtr + i * 4 + Vector256<float>.Count * 3);
+                
+                
+                Vector256<float> ps12 = Avx.DotProduct(pos256, s12, 0b0111_1000);//[2,_,_,_,1,_,_,_]
+                Vector256<float> ps34 = Avx.DotProduct(pos256, s34, 0b0111_0100);//[_,4,_,_,_,3,_,_]
+                Vector256<float> ps56 = Avx.DotProduct(pos256, s56, 0b0111_0010);//[_,_,6,_,_,_,5,_]
+                Vector256<float> ps78 = Avx.DotProduct(pos256, s78, 0b0111_0001);//[_,_,_,8,_,_,_,7]
+
+                Vector256<float> ps1234 = Avx.Or(ps12, ps34);//[2,4,_,_,1,3,_,_]
+                Vector256<float> ps5678 = Avx.Or(ps56, ps78);//[_,_,6,8,_,_,5,7]
+                Vector256<float> ps = Avx.Or(ps1234, ps5678);//[2,4,6,8,1,3,5,7]
+
+                Avx.Store(noiseValues + i, ps);
+            }
+
+
+            //Cos approximation constants
+            Vector256<float> const0_25 = Vector256.Create(0.25f);
+            Vector256<float> consttp = Vector256.Create(1.0f / (2.0f * MathF.PI));
+            Vector256<float> const16 = Vector256.Create(16.0f);
+            Vector256<float> const0_5 = Vector256.Create(0.5f);
+            Vector256<float> const_noSign = Vector256.Create(0x7fffffff).AsSingle();
+            Vector256<float> seedsCountReci = Vector256.Create(Seeds.Reci_SeedsCount);
+
             float noiseSum = 0.0f;
             float scale = 2.0f * NoiseWeight;
             for (int q = 0; q < TURBULENCE_COUNT; q++)
@@ -44,48 +76,31 @@ namespace VoxelWorld
                 {
                     break;
                 }
-
-                if (firstNoiseGen)
-                {
-                    firstNoiseGen = false;
-
-                    Vector128<float> pos128 = pos.AsVector128();
-                    Vector256<float> pos256 = Vector256.Create(pos128, pos128);
-
-                    int index = 0;
-                    for (int i = 0; i < Seeds.Seeds.Length; i += Vector256<float>.Count * 4)
-                    {
-                        //Load 8 seed vectors
-                        Vector256<float> s12 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 0);
-                        Vector256<float> s34 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 1);
-                        Vector256<float> s56 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 2);
-                        Vector256<float> s78 = Avx.LoadVector256(seedsPtr + i + Vector256<float>.Count * 3);
-                        
-                        
-                        Vector256<float> ps12 = Avx.DotProduct(pos256, s12, 0b0111_1000);//[2,_,_,_,1,_,_,_]
-                        Vector256<float> ps34 = Avx.DotProduct(pos256, s34, 0b0111_0100);//[_,4,_,_,_,3,_,_]
-                        Vector256<float> ps56 = Avx.DotProduct(pos256, s56, 0b0111_0010);//[_,_,6,_,_,_,5,_]
-                        Vector256<float> ps78 = Avx.DotProduct(pos256, s78, 0b0111_0001);//[_,_,_,8,_,_,_,7]
-
-                        Vector256<float> ps1234 = Avx.Or(ps12, ps34);//[2,4,_,_,1,3,_,_]
-                        Vector256<float> ps5678 = Avx.Or(ps56, ps78);//[_,_,6,8,_,_,5,7]
-                        Vector256<float> ps = Avx.Or(ps1234, ps5678);//[2,4,6,8,1,3,5,7]
-
-                        Avx.Store(prods + index, ps);
-                        index += Vector256<float>.Count;
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < Seeds.GetSeedsCount(); i += Vector256<float>.Count)
-                    {
-                        var fgre = Avx.LoadVector256(prods + i);
-                        Avx.Store(prods + i, Avx.Add(fgre, fgre));
-                    }
-                }
-
                 scale *= 0.5f;
-                noiseSum += scale * XYZRandomGen.GetNoise(Seeds, prods);
+
+                Vector256<float> noise = Vector256<float>.Zero;
+                for (int i = 0; i < Seeds.GetSeedsCount(); i += Vector256<float>.Count)
+                {
+                    //Load noise values
+                    Vector256<float> noises = Avx.LoadVector256(noiseValues + i);
+
+                    //Cos approximation
+                    Vector256<float> cosApprox = Avx.Multiply(noises, consttp);
+                    cosApprox = Avx.Subtract(cosApprox, Avx.Add(const0_25, Avx.Floor(Avx.Add(cosApprox, const0_25))));
+                    cosApprox = Avx.Multiply(Avx.Multiply(const16, cosApprox), Avx.Subtract(Avx.And(cosApprox, const_noSign), const0_5));
+
+                    //Sum cos approximations
+                    noise = Avx.Add(noise, cosApprox);
+
+                    //Modify noise so the turbulence noise looks random
+                    Vector256<float> turbulentNoise = Avx.Add(noises, noises);
+                    Avx.Store(noiseValues + i, turbulentNoise);
+                }
+
+                noise = Avx.DotProduct(noise, seedsCountReci, 0b1111_0001);
+                Vector128<float> lower = noise.GetLower();
+                Vector128<float> upper = noise.GetUpper();
+                noiseSum += scale * Avx.Add(lower, upper).GetElement(0);
             }
 
             return noiseSum;
